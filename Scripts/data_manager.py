3a2d8ad294b1658375cc68110a2520b6f0069fdb
AdieCode <Adiecode4@gmail.com>
2023-07-18 21:40:20 +0200

    added function to clean pollution_data_raw.csv

--- Scripts/data_manager.py
+++ Scripts/data_manager.py
@@ -29,7 +29,7 @@ def convert_unix_to_utc(unix_time, timezone_offset):
 
 
 
-def CleanDataframe(df):
+def CleanWeatherDataframe(df):
     """
     function that will create a whole new clean dataframe
     """
@@ -59,13 +59,53 @@ def CleanDataframe(df):
                    "wind_speed" : wind_data["speed"],
                    "wind_deg" : wind_data["deg"],
                    "clouds" : clouds_data["all"],
-                   "date" : convert_unix_to_utc(row["dt"],row["timezone"]),}
+                   "date" : convert_unix_to_utc(row["dt"],row["timezone"]),
+                   "country" : sys_data["country"],
+                   "city" : row["name"],}
         
         new_df = new_df._append(new_row, ignore_index=True)
 
 
     new_df.to_csv("Data/weather.csv",index=False)
 
-df = pd.read_csv("Data/weather_data_raw.csv")
 
-CleanDataframe(df)
+
+def CleanCO2Dataframe(df):
+    """
+    function that will create a whole new clean dataframe
+    """
+    new_df = pd.DataFrame()
+
+
+    # Iterate over the rows
+    for index, row in df.iterrows():
+
+        # Parse the JSON string in the "weather" column
+        list_data = json.loads(fixJson(row, "list"))
+        coord_data = json.loads(fixJson(row,"coord"))
+
+        # Extract the "description" field and assign it to a new column
+        new_row = {"aqi": list_data[0]["main"]["aqi"],
+                   "no" : list_data[0]["components"]['no'],
+                   "no2" : list_data[0]["components"]['no2'],
+                   "o3" : list_data[0]["components"]['o3'],
+                   "so2" : list_data[0]["components"]['so2'],
+                   "pm2_5" : list_data[0]["components"]['pm2_5'],
+                   "pm10" : list_data[0]["components"]['pm10'],
+                   "nh3" : list_data[0]["components"]['nh3'],
+                   "date" :  convert_unix_to_utc(list_data[0]["dt"],7200),
+                   "lat" : coord_data["lat"],
+                   "lon" : coord_data["lon"],
+                   }
+        
+        new_df = new_df._append(new_row, ignore_index=True)
+
+
+    new_df.to_csv("Data/pollution.csv",index=False)
+
+
+# control 
+weather_df = pd.read_csv("Data/weather_data_raw.csv")
+co2_df = pd.read_csv("Data/pollution_data_raw.csv")
+CleanWeatherDataframe(weather_df)
+CleanCO2Dataframe(co2_df)
