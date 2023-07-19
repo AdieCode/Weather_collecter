af4dbc5d54fede9cc87fcf5db14860af08c8b8f8
AdieCode <Adiecode4@gmail.com>
2023-07-20 00:49:52 +0200

    added a function and some progress tracking

--- Scripts/data_manager.py
+++ Scripts/data_manager.py
@@ -2,6 +2,8 @@ import pandas as pd
 import datetime
 import pytz
 import json
+from tqdm import tqdm
+from geopy.geocoders import Nominatim
 
 def fixJson(row,column):
     # Fix JSON string by replacing single quotes with double quotes
@@ -29,6 +31,22 @@ def convert_unix_to_utc(unix_time, timezone_offset):
 
 
 
+def get_location(latitude, longitude):
+
+    # Create a geolocator object
+    geolocator = Nominatim(user_agent="weather_collecter")
+
+    # Reverse geocode the coordinates
+    location = geolocator.reverse((latitude, longitude), exactly_one=True)
+
+    #get the city in the location
+    city = location.raw['address'].get('city', '')
+
+    # Return the city
+    return city
+
+
+
 def CleanWeatherDataframe(df):
     """
     function that will create a whole new clean dataframe
@@ -37,7 +55,8 @@ def CleanWeatherDataframe(df):
 
 
     # Iterate over the rows
-    for index, row in df.iterrows():
+    for index, row in tqdm(df.iterrows(), desc="Cleaning", unit="Operations",total=len(df)):
+    #for index, row in df.iterrows():
 
         # Parse the JSON string in the "weather" column
         weather_data = json.loads(fixJson(row,"weather"))
@@ -67,6 +86,7 @@ def CleanWeatherDataframe(df):
 
 
     new_df.to_csv("Data/weather.csv",index=False)
+    print("Cleaned...")
 
 
 
@@ -78,7 +98,7 @@ def CleanCO2Dataframe(df):
 
 
     # Iterate over the rows
-    for index, row in df.iterrows():
+    for index, row in tqdm(df.iterrows(), desc="Cleaning", unit="Operations",total=len(df)):
 
         # Parse the JSON string in the "weather" column
         list_data = json.loads(fixJson(row, "list"))
@@ -102,10 +122,29 @@ def CleanCO2Dataframe(df):
 
 
     new_df.to_csv("Data/pollution.csv",index=False)
+    print("Cleaned...")
+    return new_df
 
 
+def addLocation(df):
+    """
+    @recieve (df <-- DataFrame, data <-- Dictionary)
+    @return  (None)
+    """
+    #creating a new dataframe
+    df_new = df
+    New_column = []
+    
+    #iterating through the current dataframe
+    for index, row in tqdm(df_new.iterrows(), desc="Adding", unit="Locations",total=len(df_new)):
+        New_column.append(get_location(row["lat"],row["lon"]))
+
+    df_new["city"]  = New_column
+    
+    df_new.to_csv("Data/pollution.csv",index=False)
 # control 
 weather_df = pd.read_csv("Data/weather_data_raw.csv")
 co2_df = pd.read_csv("Data/pollution_data_raw.csv")
 CleanWeatherDataframe(weather_df)
-CleanCO2Dataframe(co2_df)
+df_co2 = CleanCO2Dataframe(co2_df)
+addLocation(df_co2)
